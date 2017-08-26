a33a3bd2d5b968fac692936edb2d6f7e7408ef48
AkhilRaja <p.akhilraja@gmail.com>
2017-08-26 18:00:17 +0530

    SplashScreen Added

--- app/src/main/java/arkratos/gamedev/com/colirfy/SplashScreen.java
+++ app/src/main/java/arkratos/gamedev/com/colirfy/SplashScreen.java
@@ -1,13 +1,15 @@
 package arkratos.gamedev.com.colirfy;
 
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.MediaController;
 import android.widget.VideoView;
-
-import com.bumptech.glide.Glide;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -16,12 +18,32 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.Artself);
 
-        VideoView mVideoView  = (VideoView)findViewById(R.id.videoView);
-        mVideoView.setMediaController(new MediaController(this));
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.artself);
+
+        final FrameLayout frameLayout = (FrameLayout) findViewById(R.id.frame);
+
+        final VideoView mVideoView  = (VideoView)findViewById(R.id.videoView);
+        //mVideoView.setMediaController(new MediaController(this));
         mVideoView.setVideoURI(uri);
-        mVideoView.requestFocus();
-        mVideoView.start();
+
+        //mVideoView.start();
+
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mVideoView.requestFocus();
+                mVideoView.start();
+                frameLayout.setVisibility(View.GONE);
+
+            }
+        });
+
+        mVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            public void onCompletion(MediaPlayer mp) {
+                Intent intent = new Intent(getApplicationContext(),FirstActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
